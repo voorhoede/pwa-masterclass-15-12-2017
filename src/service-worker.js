a58242b064cd6dd364468cee2dfd7ad265ab6c57
decrek <declanrek@gmail.com>
2017-12-15 13:54:35 +0100

    add sw helpers

--- src/service-worker.js
+++ src/service-worker.js
@@ -7,8 +7,32 @@ self.addEventListener('fetch', event => {
 });
 
 /**
- * This is boilerplate, instructing the service worker to take control as soon
- * as it can.
+ * Checks if a request is a core GET request
+ *
+ * @param {Object} request		The request object
+ * @returns {Boolean}			Boolean value indicating whether the request is in the core mapping
  */
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', () => self.clients.claim());
+function isCoreGetRequest(request) {
+    return request.method === 'GET' && CORE_ASSETS.includes(getPathName(request.url));
+}
+
+/**
+ * Checks if a request is a GET and HTML request
+ *
+ * @param {Object} request		The request object
+ * @returns {Boolean}			Boolean value indicating whether the request is a GET and HTML request
+ */
+function isHtmlGetRequest(request) {
+    return request.method === 'GET' && (request.headers.get('accept') !== null && request.headers.get('accept').indexOf('text/html') > -1);
+}
+
+/**
+ * Get a pathname from a full URL by stripping off domain
+ *
+ * @param {Object} requestUrl		The request object, e.g. https://www.mydomain.com/index.css
+ * @returns {String}			    Relative url to the domain, e.g. index.css
+ */
+function getPathName(requestUrl) {
+    const url = new URL(requestUrl);
+    return url.pathname;
+}
